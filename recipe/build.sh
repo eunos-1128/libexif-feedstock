463ce1216c591e731479325224fa842e7edfbb5d
UENO, M. <eunosium-1128@live.jp>
2026-05-29 00:54:25 +0900

    Fix autoconf command in build script

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 # Get an updated config.sub and config.guess
 cp ${BUILD_PREFIX}/share/gnuconfig/config.* .
 
-AUTOPOINT=true autoreconf -vfi
+autoreconf -vfi
 ./configure --prefix=${PREFIX} --disable-static --enable-shared --enable-pic --disable-nls --disable-dependency-tracking
 make -j${CPU_COUNT}
 if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" ]]; then
