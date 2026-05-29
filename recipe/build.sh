5d15beeed4e165415d04449a3e25569d2294c183
UENO, M. <eunosium-1128@live.jp>
2026-05-29 17:59:30 +0900

    Add windows support (#5)
    * Update recipe.yaml to include autotools for Windows
    
    * Add build.bat for autotools build process
    
    * MNT: Re-rendered with conda-smithy 3.62.0 and conda-forge-pinning 2026.05.29.03.36.5
    
    * Update build.sh
    
    * Update build.sh
    
    * Update recipe.yaml
    
    * Update recipe.yaml
    
    * Fix ssize_t definition for Windows in exif-loader.c
    
    Replace ssize_t with ptrdiff_t for Windows compatibility.
    
    * Add patch reference to recipe.yaml
    
    * Fix build script for cross-compilation on Windows
    
    * Increment build number from 1 to 2
    
    * Refine platform checks in build script
    
    Update target platform check for Windows and cross-compilation.
    
    * Enhance build script for Windows libtool patching
    
    Add support for patching libtool in subdirectories on Windows.
    
    * Simplify Windows target platform checks in build.sh
    
    Refactor Windows platform handling in build script.
    
    * Update build.sh to check target platform for cross-compilation
    
    * Modify build script to enable verbose output
    
    * Add Windows-specific DLL handling in build script
    
    ---------
    
    Co-authored-by: conda-forge-webservices[bot] <91080706+conda-forge-webservices[bot]@users.noreply.github.com>

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,13 +1,29 @@
 #!/bin/bash
-set -euo pipefail
+set -exo pipefail
 
-# Get an updated config.sub and config.guess
-cp ${BUILD_PREFIX}/share/gnuconfig/config.* .
+if [[ "${target_platform}" != "win-"* ]]; then
+  # Get an updated config.sub and config.guess
+  cp ${BUILD_PREFIX}/share/gnuconfig/config.* .
+fi
 
-autoreconf -vfi
-./configure --prefix=${PREFIX} --disable-static --enable-shared --enable-pic --disable-nls --disable-dependency-tracking
+./configure --prefix=${PREFIX} --disable-static --enable-shared --disable-nls --disable-dependency-tracking
+
+if [[ "${target_platform}" == win-* ]]; then
+    patch_libtool
+    # `-export-symbols` is not supported by lld-link on Windows;
+    # replace with `-export-symbols-regex` to export all symbols
+    sed -i.bak 's|-export-symbols $(srcdir)/libexif.sym|-export-symbols-regex ".*"|g' libexif/Makefile
+fi
+
 make -j${CPU_COUNT}
-if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" ]]; then
+
+if [[ "${target_platform}" != "win-"* && "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" ]]; then
   make check
 fi
+
 make install
+
+if [[ "${target_platform}" == "win-"* ]]; then
+    mv "${PREFIX}"/bin/exif-*.dll "${PREFIX}/bin/exif.dll"
+    mv "${PREFIX}/lib/exif.dll.lib" "${PREFIX}/lib/exif.lib"
+fi
