3a80958d1c2d620b50a4a4801b7206b7a11a20ba
UENO, M. <eunosium-1128@live.jp>
2026-05-29 17:04:49 +0900

    Enhance build script for Windows libtool patching
    Add support for patching libtool in subdirectories on Windows.

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,7 +7,17 @@ if [[ "${target_platform}" != "win-"* ]]; then
 fi
 
 ./configure --prefix=${PREFIX} --disable-static --enable-shared --disable-nls --disable-dependency-tracking
-[[ "${target_platform}" == "win-"* ]] && patch_libtool
+
+if [[ "${target_platform}" == "win-"* ]]; then
+    # `patch_libtool` only patches the libtool in the current directory;
+    # run it in each subdirectory that contains a libtool script
+    for lt in $(find . -name "libtool" ! -name "*.bak"); do
+        pushd "$(dirname "$lt")"
+        patch_libtool
+        popd
+    done
+fi
+
 make -j${CPU_COUNT}
 
 if [[ "${target_platform}" != "win-"* && "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" ]]; then
