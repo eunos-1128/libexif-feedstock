c2c85176a2795dec8f96d88f6324d6dbfe1eccf4
UENO, M. <eunosium-1128@live.jp>
2026-05-29 16:15:52 +0900

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 set -euo pipefail
 
-# Get an updated config.sub and config.guess
-cp ${BUILD_PREFIX}/share/gnuconfig/config.* .
+if [[ "${target_platform}" != "win-"* ]]; then
+  # Get an updated config.sub and config.guess
+  cp ${BUILD_PREFIX}/share/gnuconfig/config.* .
+fi
+
 
 autoreconf -vfi
-./configure --prefix=${PREFIX} --disable-static --enable-shared --enable-pic --disable-nls --disable-dependency-tracking
+./configure --prefix=${PREFIX} --disable-static --enable-shared --disable-nls --disable-dependency-tracking
 make -j${CPU_COUNT}
 if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" ]]; then
   make check
