1619d6f3c22fca547b9bf7acb109eca3af9be6e8
UENO, M. <eunosium-1128@live.jp>
2026-05-29 16:57:02 +0900

    Refine platform checks in build script
    Update target platform check for Windows and cross-compilation.

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,9 +7,11 @@ if [[ "${target_platform}" != "win-"* ]]; then
 fi
 
 ./configure --prefix=${PREFIX} --disable-static --enable-shared --disable-nls --disable-dependency-tracking
-[[ "${target_platform}" == win-* ]] && patch_libtool
+[[ "${target_platform}" == "win-"* ]] && patch_libtool
 make -j${CPU_COUNT}
-if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" ]]; then
+
+if [[ "${target_platform}" != "win-"* && "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" ]]; then
   make check
 fi
+
 make install
